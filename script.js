29f7d6948914cf66012f273d2da9a60efde2ae46
Hector DelaRosa <hdelaros@gmail.com>
2020-07-31 22:02:41 -0400

    Finished pagination functionality and styling

--- script.js
+++ script.js
@@ -1,80 +1,56 @@
-window
-  .fetch('https://acme-users-api-rev.herokuapp.com/api/users')
-  .then(response =>
-    response.json().then(data => {
-      let html = data.users
-        .map(user => {
-          return `<div class="user">
-            <div>${user.firstName}</div>
-            <div>${user.lastName}</div>
-            <div>${user.email}</div>
-            <div>${user.title}</div>
-          </div>`;
-        })
-        .join('');
-        html = `<div class="header">
-          <div>First Name</div>
-          <div>Last Name</div>
-          <div>Email</div>
-          <div>Title</div>
-        </div>${html}`;
-      document.querySelector('#usersList').innerHTML = html;
+let pager = document.querySelector('#pager');
+let usersList = document.querySelector('#usersList');
+let hash = window.location.hash.slice(1);
+let linksNodeList;
+const renderUserList = (data) => {
+  return data.users
+    .map((user) => {
+      return `<li class="user">
+          <div>${user.firstName}</div>
+          <div>${user.lastName}</div>
+          <div>${user.email}</div>
+          <div>${user.title}</div>
+        </li>`;
     })
-  );
+    .join('');
+};
 
+const handleHashChange = (hashValue) => {
+  linksNodeList.forEach((ele, idx) => {
+    if (Number(hashValue) === idx) {
+      ele.classList.add('active');
+    } else {
+      ele.classList.remove('active');
+    }
+  });
 
+  if (hashValue === '0') {
+    hashValue = '';
+  }
+  window
+    .fetch(`https://acme-users-api-rev.herokuapp.com/api/users/${hashValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      usersList.innerHTML = renderUserList(data);
+    });
+};
 
+window
+  .fetch(`https://acme-users-api-rev.herokuapp.com/api/users/${hash}`)
+  .then((response) => response.json())
+  .then((data) => {
+    let links = '';
+    for (let i = 0; i < Math.ceil(data.count / 50); i++) {
+      links += `<a href='#${i}'>${i + 1}</a>`;
+    }
+    pager.innerHTML = links;
+    usersList.innerHTML = renderUserList(data);
+    linksNodeList = document.querySelectorAll('a');
+    // linksNodeList.forEach((ele) => {
+    //   ele.addEventListener('click', (ev) => {});
+    // });
+  });
 
-
-
-
-
-
-
-
-
-
-
-// // const users = myFunction();
-// // const usersArray = users.then(data => data.users).then(value => render(value))
-
-// // console.log(usersArray);
-// async function myFunction() {
-//   const response = await fetch(
-//     'https://acme-users-api-rev.herokuapp.com/api/users'
-//   );
-//   const json = await response.json(); // convert response to JSON data; .json() also needs the await keyword
-//   // console.log(json);
-//   // return json;
-
-//   //const users = myFunction();
-//   const users = json;
-//   // const usersArray = users.then(data => data.users); //.then(value => render(value));
-//   c;
-//   function render(arr) {
-//     let html = arr.map(user => {
-//       `<div class=userContainer>
-
-//             <div>
-//             ${user.firsName}
-//             </div>
-//             <div>
-//             ${user.lastName}
-//             </div>
-//             <div>
-//             ${user.email}
-//             </div>
-//             <div>
-//             ${user.title}
-//             </div>
-
-//             </div>
-//             `;
-//     });
-//     console.log(html);
-//   }
-
-//   render(users);
-// }
-// myFunction();
-// // render(usersArray);
+window.addEventListener('hashchange', () =>
+  handleHashChange(window.location.hash.slice(1))
+);
